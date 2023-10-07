29feee91a08d19d38b612fa2a793ff7134604109
lqzh <9134671+lqzhgood@users.noreply.github.com>
2023-10-07 17:05:59 +0800

    ✨ feat(UseCallback):

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,19 @@ import Count from './demo/Count';
 import List from './demo/List';
 import UseRefFC from './demo/UseRef';
 import UseMemo from './demo/UseMemo.demo';
+import UseCallback from './demo/UseCallback';
 
 function App() {
-	const count = useState();
-	return (
-		<div className='App'>
-			{/* <List /> */}
-			{/* <Count init={100} /> */}
-			{/* <UseRefFC /> */}
-			<UseMemo />
-		</div>
-	);
+    const count = useState();
+    return (
+        <div className='App'>
+            {/* <List /> */}
+            {/* <Count init={100} /> */}
+            {/* <UseRefFC /> */}
+            {/* <UseMemo /> */}
+            <UseCallback />
+        </div>
+    );
 }
 
 export default App;
--- src/demo/UseCallback.tsx
+++ src/demo/UseCallback.tsx
@@ -1,5 +1,16 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
 const UseCallback = () => {
-    return <div>UseCallback</div>;
+    const [text, setText] = useState('hello');
+
+    const fn1 = () => console.log('fn1', text);
+    const fn2 = useCallback(() => console.log('fn2', text), [text]);
+
+    return (
+        <div>
+            <input type='text' value={text} onChange={event => setText(event.target.value)} />
+        </div>
+    );
 };
+
+export default UseCallback;
