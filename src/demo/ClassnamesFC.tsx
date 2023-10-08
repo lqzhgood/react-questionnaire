e357a08063ecd6f48e361ec4f4255511b54f1615
lqzh <9134671+lqzhgood@users.noreply.github.com>
2023-10-08 09:55:24 +0800

    ✨ feat(css): module

--- src/demo/ClassnamesFC.tsx
+++ src/demo/ClassnamesFC.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import classnames from 'classnames';
 import './ClassnamesFC.css';
+import style from './ClassnamesFC.module.css';
 
 const classnamesFC = () => {
     const [push, setPush] = useState(false);
 
-    const pushClass = classnames({ push: push });
+    const pushClass = classnames({ push: push, [style['push-module']]: push });
 
     return (
         <div
