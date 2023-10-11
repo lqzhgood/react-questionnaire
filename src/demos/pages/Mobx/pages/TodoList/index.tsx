3b5510887b669a1d873eb2c1311ebe7deac2cfe2
lqzh <9134671+lqzhgood@users.noreply.github.com>
2023-10-11 20:01:49 +0800

    ✨ feat(demos): Mobx completed todos

--- src/demos/pages/Mobx/pages/TodoList/index.tsx
+++ src/demos/pages/Mobx/pages/TodoList/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import store from './store';
 import TodoListFC from './TodoListFC';
+import { observer } from 'mobx-react';
 
-const TodoList = () => {
+const TodoList = observer(() => {
     return (
         <div>
             <h2>TodoList</h2>
+            <p>completed: {store.completedTodosCount}</p>
             <TodoListFC store={store} />
         </div>
     );
-};
+});
 
 export default TodoList;
