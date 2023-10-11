6a23184951d82ededd92c75c7609774eca785bae
lqzh <9134671+lqzhgood@users.noreply.github.com>
2023-10-11 10:56:19 +0800

    ✨ feat(page): userInfo

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,17 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { LOGIN_PATHNAME } from '../routers';
+import React, { useMemo } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { HOME_PATHNAME, LOGIN_PATHNAME } from '../routers';
+import { useRequest } from 'ahooks';
+import { getUserInfoService } from '@/services/user';
+import { UserOutlined } from '@ant-design/icons';
+import { Button, message } from 'antd';
+import { removeToken } from '@/utils/user-token';
 
-type Props = {
-    //
-};
+// TODO 未登录不显示
+const UserInfo = () => {
+    const nav = useNavigate();
+    const { data } = useRequest(getUserInfoService);
+    const { username, nickname } = data || {};
 
-const UserInfo = (props: Props) => {
-    return (
-        <>
-            <Link to={LOGIN_PATHNAME}>登录</Link>
-        </>
-    );
+    function logout() {
+        removeToken();
+        nav(HOME_PATHNAME);
+        message.success('退出成功');
+    }
+
+    const UserInfo = useMemo(() => {
+        return (
+            <>
+                <span style={{ color: '#e8e8e8' }}>
+                    <UserOutlined />
+                    {nickname}
+                </span>
+                <Button type='link' onClick={logout}>
+                    退出
+                </Button>
+            </>
+        );
+    }, [username, nickname]);
+
+    return username ? UserInfo : <Link to={LOGIN_PATHNAME}>登录</Link>;
 };
 
 export default UserInfo;
