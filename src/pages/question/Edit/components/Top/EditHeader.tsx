d337fe23d1b4f850e5b2b819f38c5aed75c8c620
lqzh <9134671+lqzhgood@users.noreply.github.com>
2023-10-15 22:10:15 +0800

    ✨ feat(Edit): EditHeader title

--- src/pages/question/Edit/components/Top/EditHeader.tsx
+++ src/pages/question/Edit/components/Top/EditHeader.tsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './EditHeader.module.sass';
-import { Button, Space, Typography } from 'antd';
-import { LeftOutlined } from '@ant-design/icons';
+import { Button, Input, Space, Typography } from 'antd';
+import { EditOutlined, LeftOutlined } from '@ant-design/icons';
 import { useNavigate } from 'react-router-dom';
 import EditToolbar from './EditToolbar';
+import useGetQuestionPageInfo from '@/hooks/useGetQuestionPageInfo';
+import { useDispatch } from 'react-redux';
+import { updatePageInfo } from '@/store/pageInfo';
 
 const { Title } = Typography;
+
+const TitleElm = () => {
+    const { pageInfo } = useGetQuestionPageInfo();
+    const { title } = pageInfo;
+    const dispatch = useDispatch();
+
+    const [editState, setEditState] = useState(false);
+
+    return (
+        <Space align='baseline'>
+            {!editState ? (
+                <Title className={styles.title}>{title}</Title>
+            ) : (
+                <Input
+                    value={title}
+                    onChange={e => {
+                        const t = e.target.value.trim();
+                        if (!t) return;
+                        dispatch(updatePageInfo({ title: t }));
+                    }}
+                    onBlur={() => setEditState(false)}
+                    onPressEnter={() => setEditState(false)}
+                />
+            )}
+            <Button type='text' icon={<EditOutlined />} onClick={() => setEditState(true)}></Button>
+        </Space>
+    );
+};
 
 const EditHeader = () => {
     const nav = useNavigate();
@@ -23,7 +54,7 @@ const EditHeader = () => {
                         >
                             返回
                         </Button>
-                        <Title className={styles.title}>问卷标题</Title>
+                        <TitleElm />
                     </Space>
                 </div>
                 <div className={styles.main}>
