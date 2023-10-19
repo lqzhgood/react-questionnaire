7ab1b140b36c825f253636dbf25bedc847d94c87
lqzh <9134671+lqzhgood@users.noreply.github.com>
2023-10-19 14:51:59 +0800

    ✨ feat(demos): add demos entry

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
-import { Layout } from 'antd';
+import { Link, Outlet } from 'react-router-dom';
+import { Layout, Space } from 'antd';
 const { Header, Content, Footer } = Layout;
 
 import styles from './MainLayout.module.sass';
@@ -8,6 +8,7 @@ import Logo from '../components/Logo';
 import UserInfo from '../components/UserInfo';
 import { WEB_NAME } from '../const/web';
 import useNavPage from '@/hooks/useNavPage';
+import { AppstoreAddOutlined } from '@ant-design/icons';
 
 const MainLayout = () => {
     useNavPage();
@@ -25,7 +26,14 @@ const MainLayout = () => {
                 <Outlet />
             </Content>
             <Footer className={styles.footer}>
-                {WEB_NAME} &copy; {new Date().getFullYear()}
+                <Space>
+                    <span>
+                        {WEB_NAME} &copy; {new Date().getFullYear()}
+                    </span>
+                    <Link to='/demos'>
+                        <AppstoreAddOutlined title='Demos' />
+                    </Link>
+                </Space>
             </Footer>
         </Layout>
     );
