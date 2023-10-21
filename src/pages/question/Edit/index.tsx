bab4b10db10484b2a228fc4832fe3f81606dcc1f
lqzh <9134671+lqzhgood@users.noreply.github.com>
2023-10-21 20:44:20 +0800

    ✨ feat(Edit): add title

--- src/pages/question/Edit/index.tsx
+++ src/pages/question/Edit/index.tsx
@@ -9,9 +9,13 @@ import EditCanvas from './components/Main/EditCanvas';
 import LeftPanel from './components/Left/LeftPanel';
 import RightPanel from './components/Right/RightPanel';
 import EditHeader from './components/Top/EditHeader';
+import { useTitle } from 'ahooks';
+import useGetQuestionPageInfo from '@/hooks/useGetQuestionPageInfo';
 
 const Edit = () => {
     const { loading } = useLoadQuestionData();
+    const { title } = useGetQuestionPageInfo();
+    useTitle(title + '- 问卷编辑', { restoreOnUnmount: true });
 
     const dispatch = useDispatch();
 
