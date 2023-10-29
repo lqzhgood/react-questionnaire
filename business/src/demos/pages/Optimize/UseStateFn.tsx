51cb906d66fc26ab5da71d22b13d30329bca8ff0
lqzh <9134671+lqzhgood@users.noreply.github.com>
2023-10-29 21:32:05 +0800

    ✨ feat(demos): optimize-useStateFn

--- business/src/demos/pages/Optimize/UseStateFn.tsx
+++ business/src/demos/pages/Optimize/UseStateFn.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-type Props = {
-    //
-};
+const p = new Proxy([], {
+    get(target, p, receiver) {
+        console.log('proxy get');
+        if (p === 'length') {
+            return target.length;
+        }
 
-const UseStateFn = (props: Props) => {
-    return <div>UseStateFn</div>;
+        if (p === 'join') {
+            return 'xxx';
+        }
+
+        return 'proxy';
+    },
+    set(target: string[], p, newValue, receiver) {
+        target.push(newValue);
+        console.log('proxy set');
+        return true;
+    },
+});
+
+const UseStateFn = () => {
+    console.log('component init');
+    const [arr, setArr] = useState(() => {
+        console.log('function init');
+        return ['init'];
+    });
+    function add() {
+        setArr([...arr, 'a']);
+    }
+
+    const [pry, setProxy] = useState(p);
+
+    return (
+        <div>
+            <h4>useState 初始化时，普通变量每次都会执行，传入函数将仅执行一次</h4>
+            <p>{arr.length}</p>
+            <p>{arr.join()}</p>
+            {pry}
+            <button onClick={add}>add</button>
+        </div>
+    );
 };
 
 export default UseStateFn;
