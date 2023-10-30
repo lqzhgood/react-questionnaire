76ee0f14a48df7720d2ef12cb8e3e812e04d518e
lqzh <9134671+lqzhgood@users.noreply.github.com>
2023-10-30 11:22:18 +0800

    🐞 fix(demos): add close

--- business/src/demos/index.tsx
+++ business/src/demos/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { demosRouter } from '@/demos/router';
-import { LinkOutlined } from '@ant-design/icons';
+import { CloseOutlined, LinkOutlined } from '@ant-design/icons';
 import { Button, Divider, Space, Typography } from 'antd';
-import { Link, Outlet } from 'react-router-dom';
-
+import { Link, Outlet, useNavigate } from 'react-router-dom';
+import styles from './index.module.sass';
 const { Title } = Typography;
 const Demos = () => {
+    const nav = useNavigate();
     return (
         <div style={{ padding: '20px' }}>
             <h1>Demos</h1>
             <p>一些 Api 的临时 Demos</p>
+            <Button className={styles.close} type='link' icon={<CloseOutlined />} onClick={() => nav('/')}></Button>
 
             {demosRouter.children.map(r => (
                 <div key={r.path} style={{ margin: '10px 0' }}>
