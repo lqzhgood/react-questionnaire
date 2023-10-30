074493f2f1089ee61fb7a15cc2b17f398c94494d
lqzh <9134671+lqzhgood@users.noreply.github.com>
2023-10-30 16:45:50 +0800

    🧪 test(Question): input textarea

---
+++ business/src/components/QuestionComponents/QuestionInput/Component.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Component from './Component';
+import { QuestionInputDefaultProps } from './interface';
+
+describe('QuestionInput', () => {
+    render(<Component />);
+
+    test('默认属性', () => {
+        const t = screen.getByText(QuestionInputDefaultProps.title || '');
+        expect(t).toBeInTheDocument();
+
+        const input = screen.getByPlaceholderText(QuestionInputDefaultProps.placeholder || '');
+        expect(input).toBeInTheDocument();
+        expect(input.matches('input')).toBeTruthy();
+    });
+
+    test('传入属性', () => {
+        const title = 'hello';
+        const placeholder = 'world';
+        render(<Component title={title} placeholder={placeholder} />);
+
+        const t = screen.getByText(title);
+        expect(t).toBeInTheDocument();
+
+        const d = screen.getByPlaceholderText(placeholder);
+        expect(d).toBeInTheDocument();
+    });
+});
---
+++ business/src/components/QuestionComponents/QuestionTextarea/Component.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Component from './Component';
+import { QuestionTextareaDefaultProps } from './interface';
+
+describe('QuestionTextarea', () => {
+    render(<Component />);
+
+    test('默认属性', () => {
+        const t = screen.getByText(QuestionTextareaDefaultProps.title || '');
+        expect(t).toBeInTheDocument();
+
+        const textArea = screen.getByPlaceholderText(QuestionTextareaDefaultProps.placeholder || '');
+        expect(textArea).toBeInTheDocument();
+        expect(textArea.matches('textarea')).toBeTruthy();
+    });
+
+    test('传入属性', () => {
+        const title = 'hello';
+        const placeholder = 'world';
+        render(<Component title={title} placeholder={placeholder} />);
+
+        const t = screen.getByText(title);
+        expect(t).toBeInTheDocument();
+
+        const d = screen.getByPlaceholderText(placeholder);
+        expect(d).toBeInTheDocument();
+    });
+});
