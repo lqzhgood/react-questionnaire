29757798ab07af87acd810c2816802ced5532df4
lqzh <9134671+lqzhgood@users.noreply.github.com>
2023-10-31 09:06:05 +0800

    ✨ feat(storybook): add entry

--- business/src/pages/Home.tsx
+++ business/src/pages/Home.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Button, Typography } from 'antd';
+import { Button, Space, Typography } from 'antd';
 import { MANAGER_INDEX_PATHNAME } from '../routers';
 
 const { Title, Paragraph } = Typography;
 
 import styles from './Home.module.sass';
+import { ReactComponent as StorybookIcon } from '@/assets/storybook.svg';
 
 const Home = () => {
     const nav = useNavigate();
@@ -15,11 +16,14 @@ const Home = () => {
             <div className={styles.info}>
                 <Title>问卷调查 | 在线投票</Title>
                 <Paragraph>已累计创建问卷 100 份，发布问卷 90 份，收到答卷 980 份</Paragraph>
-                <div>
+                <Space>
                     <Button type='primary' onClick={() => nav(MANAGER_INDEX_PATHNAME)}>
                         开始使用
                     </Button>
-                </div>
+                    <Button type='link' className={styles.storybook} onClick={() => window.open('./storybook')}>
+                        <StorybookIcon className={styles.icon} />
+                    </Button>
+                </Space>
             </div>
         </div>
     );
